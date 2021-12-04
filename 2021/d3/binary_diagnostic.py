25dc6b608141b188787e48f054e431005e3ce2d0
dtuckersuh <suh977@gmail.com>
2021-12-03 23:52:29 -0800

    day 3

--- 2021/d3/binary_diagnostic.py
+++ 2021/d3/binary_diagnostic.py
@@ -1,3 +1,4 @@
+from collections import Counter
 # Power Consumption = PC
 # PC = Gamma rate (GR) * epsilon rate (ER)
 # GR = Most common bit in each corresponding position
@@ -25,10 +26,11 @@ def power_consumption():
     er = "".join(map(str, epsilon))
     return int(gr, 2) * int(er, 2)
 
-pc = power_consumption()
-print(pc)
+# Part 1
+part1_output = power_consumption()
+print("Part 1:", part1_output)
 
-"
+'''
 - Only keep numbers selected by bit criteria
 
 - If only one number left, this is the rating value
@@ -44,7 +46,28 @@ print(pc)
 - Convert OGR and CSR from binary to decimal
 
 - Life Support Rating = OGR * CSR 
-"
+'''
 def life_support():
     report = read_file()
+    # OGR criteria = Oxygen Generator Rating
+    ogr = report
+    for i in range(len(ogr[0])):
+        count = Counter([val[i] for val in ogr])
+        if count['0'] > count['1']:
+            ogr = [num for num in ogr if num[i] == '0'] 
+        else:
+            ogr = [num for num in ogr if num[i] == '1']
 
+    # CSR criteria = CO2 Scrubber Rating
+    csr = report
+    for i in range(len(csr[0])):
+        count = Counter([val[i] for val in csr])
+        if count['0'] > count['1']:
+            csr = [num for num in csr if num[i] == '1']
+        else:
+            csr = [num for num in csr if num[i] == '0']
+        if len(csr) == 1:
+            return int(ogr[0], 2) * int(csr[0], 2)
+
+part2_output = life_support()
+print("Part 2:", part2_output)
